d73204fb796199ccfa7adb3945c6d4c79f6422c4
matiasow <matias@becloudy.com>
2013-11-26 23:18:10 -0300

    Added conference schedule

--- .generator.rb
+++ .generator.rb
@@ -19,9 +19,18 @@ class Generator
     write_html @lang + '/index.html', ERB.new( File.open(".index.html.erb").read ).result(binding)
     write_html @lang + '/program.html', ERB.new( File.open(".program.html.erb").read ).result(binding)
     write_html @lang + '/rfd.html', ERB.new( File.open(".rfd.html.erb").read ).result(binding)
+    write_html @lang + '/schedule.html', ERB.new( File.open(".schedule.html.erb").read ).result(binding)
     write_html @lang + '/app.html', ERB.new( File.open(".app.html.erb").read ).result(binding)
   end
 
+  def schedule_talk(id, rfd = false)
+    if rfd
+      "<a href=\"rfd.html##{id}\">#{@rfd_speakers[id]['title']}</a><span>#{@rfd_speakers[id]['full_name']}</span>"
+    else
+      "<a href=\"program.html##{id}\">#{@speakers[id]['title']}</a><span>#{@speakers[id]['full_name']}</span>"
+    end
+  end
+  
 private
   def write_html(file, html)
     File.open(file, 'w'){|f| f.write html}
